724a73b0d755eca3e8035c32e2f775f746847a4f
edward-yebuahokwei <teddyeb2001@gmail.com>
2023-09-03 00:32:16 +0100

    Create Security Group

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "my_public_ip" {
+  description = "My personal IP address"
+  default     = "143.159.209.21"
+}
