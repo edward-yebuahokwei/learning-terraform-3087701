7dcefa6e610a632f7b505b6c26bfd453f19e4d0e
edward-yebuahokwei <teddyeb2001@gmail.com>
2023-09-03 19:47:18 +0100

    Fix long alb name_prefix

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-alb"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
