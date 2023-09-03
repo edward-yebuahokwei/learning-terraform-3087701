17872b92944d7e3b1063decfffcc1a1c92b08ddb
edward-yebuahokwei <teddyeb2001@gmail.com>
2023-09-03 17:36:42 +0100

    Fix duplicate instance_type variable

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t3.nano"
-}
-
 variable "my_public_ip" {
   description = "My personal IP address"
   default     = "0.0.0.0/0"
