0e58ef553d8f32c734f51b3b16e104a6fc368a06
edward-yebuahokwei <teddyeb2001@gmail.com>
2023-09-03 17:35:31 +0100

    Refactor to use variables

--- variables.tf
+++ variables.tf
@@ -7,3 +7,46 @@ variable "my_public_ip" {
   description = "My personal IP address"
   default     = "0.0.0.0/0"
 }
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
+}
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+
+  type = object ({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631"
+  }
+}
+
+variable "environment" {
+  description = "Development Environment"
+
+  type = object ({
+    name            = string
+    network_prefix  = string
+  })
+
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "asg_min_size" {
+  description = "Minimun number of instances in the ASG"
+  default     = 1
+}
+
+variable "asg_max_size" {
+  description = "Maximum number of instances in the ASG"
+  default     = 2
+}
